dc5414fda8bc4c98c09056687e491d5046dd84ae
emmanuel <emma_dj_nash@hotmail.com>
2014-08-05 09:34:50 -0500

    VERSION 1.5
    Se le agrega AUDIO a la aplicacion

--- js/acciones.js
+++ js/acciones.js
@@ -1,13 +1,26 @@
 //JavaScript
+	var audio ;
 	
 $(document).ready(function(e){
 	
+	if(window.plugin && window.plugins.lowlatencyAudio)
+	
+	{
+		audio=window.plugins.lowlatencyAudio;
+		audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		}//Precargar el PLAY
+		
+		
 	$("#beep").click(function(e){
 		navigator.notification.beep(3);
 	});//click beep
 	
 	$("#vibrar").click(function(e){
 		navigator.notification.vibrate(1000);
+	});//click beep
+	
+	$("#play").click(function(e){
+		audio.play('mario');
 	});//click beep
 	
 });//ready
