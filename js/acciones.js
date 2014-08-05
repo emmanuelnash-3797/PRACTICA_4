bff10f44aca9d688d7b356bfaeabdc47cfa5f7ad
emmanuel <emma_dj_nash@hotmail.com>
2014-08-05 07:23:28 -0500

    VERSION 1.1
    Primera version del aplicacion movil

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+//JavaScript
+	
+$(document).ready(function(e){
+	
+	$("#beep").click(function(e){
+		alert('click en BEEP');
+	});//click beep
+	
+	$("#vibrar").click(function(e){
+		alert('click en VIBRAR');
+	});//click beep
+	
+});//ready
