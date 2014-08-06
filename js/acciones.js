9a155b062cd5ddd79113b1876262930705f3689d
emmanuel martinez blas <dj.nash.emmanuel@gmail.com>
2014-08-06 08:26:34 -0500

    VERSION 1.5.4
    CORRECCION

--- js/acciones.js
+++ js/acciones.js
@@ -5,7 +5,7 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		
 	
-		audio=window.plugins.lowlatencyAudio;
+		audio=window.plugins.LowLatencyAudio;
 		audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert ('error: '+msg);});
 		
 		
