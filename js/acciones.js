21254e06556fe0177aef75e5a5a88290d79fe557
emmanuel martinez blas <dj.nash.emmanuel@gmail.com>
2014-08-06 08:23:35 -0500

    VERSION 1.5.3
    CORECCION

--- js/acciones.js
+++ js/acciones.js
@@ -2,25 +2,24 @@
 	var audio ;
 	
 $(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		
 	
-	if(window.plugin && window.plugins.lowlatencyAudio)
-	
-	{
 		audio=window.plugins.lowlatencyAudio;
 		audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert ('error: '+msg);});
-		}//Precargar el PLAY
 		
 		
-	$("#beep").bind("tap")(function(e){
+	$("#beep").bind("tap",function(e){
 		navigator.notification.beep(3);
 	});//click beep
 	
-	$("#vibrar").bind("tap")(function(e){
+	$("#vibrar").bind("tap",function(e){
 		navigator.notification.vibrate(1000);
 	});//click beep
 	
-	$("#play").bind("tap")(function(e){
+	$("#play").bind("tap",function(e){
 		audio.play('mario');
 	});//click beep
 	
-});//ready
+	});//ready
+	});//ready
